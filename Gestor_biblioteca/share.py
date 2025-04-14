7d844bb61f4fe85d59e30377bfdb6f6d0127a230
arachoxs <140642177+arachoxs@users.noreply.github.com>
2025-04-14 13:10:12 -0500

    Delete Gestor_biblioteca directory

--- Gestor_biblioteca/share.py
+++
@@ -1,8 +0,0 @@
-def pedir_entero(mensaje):
-    while True:
-        try:
-            valor = int(input(mensaje))
-            return valor
-        except ValueError:
-            print("Entrada inválida. Por favor, ingrese un número entero.")
-
